d606ee6b7905fcb99dafec830086a4451c3ded56
berz20 <91781284+berz20@users.noreply.github.com>
2023-10-15 21:33:19 +0200

    lockscreen + suspend lid switch

---
+++ hypr/scripts/switch_lid.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if grep open /proc/acpi/button/lid/LID0/state; then
+	hyprctl keyword monitor "eDP-1, preferred, auto, 1"
+else
+	if [[ $(hyprctl monitors | grep "Monitor" | wc -l) != 1 ]]; then
+		hyprctl keyword monitor "eDP-1, disable"
+	else
+		swaylock -f
+
+	fi
+fi
